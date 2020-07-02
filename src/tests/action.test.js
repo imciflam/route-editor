59f219096440860e98a41a44b3c1c6065d2a6ad8
Vita <vitalia@myrambler.ru>
2020-07-02 21:06:08 +0300

    + more tests

--- src/tests/action.test.js
+++ src/tests/action.test.js
@@ -1,4 +1,4 @@
-import * as actions from '../actions/index';
+import * as actions from '../actions/';
 import * as types from '../actions/types';
 
 describe('Create marker action', () => {
@@ -24,5 +24,30 @@ describe('Remove marker action', () => {
             id: id
         };
         expect(actions.removeMarker(id)).toEqual(expectedAction);
+    });
+});
+
+describe('Change position action', () => {
+    it('should add an action to change position of the marker by id', () => {
+        const randId = Math.floor(Math.random() * 1000)
+        const position = { lat: 55.6, lng: 51.8 };
+        const expectedAction = {
+            type: types.CHANGE_POSITION,
+            id, position
+        };
+        expect(actions.changePosition(randId, position)).toEqual(expectedAction);
+    });
+});
+
+
+describe('Change order action creator', () => {
+    it('should add an action to change order of markers', () => {
+        const randPositionId = Math.floor(Math.random() * 100)
+        const randId = Math.floor(Math.random() * 1000)
+        const expectedAction = {
+            type: types.CHANGE_ORDER,
+            randPositionId, randId
+        };
+        expect(actions.changeOrder(randPositionId, randId)).toEqual(expectedAction);
     });
 });
