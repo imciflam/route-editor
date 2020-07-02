9ab2be3122cb1c49b28e033f822c978b7e658d5d
Vita <vitalia@myrambler.ru>
2020-07-02 20:26:21 +0300

    Updates

---
+++ precache-manifest.6ac0f6597a3f66b02ec8c3a107ca84ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402e3265067b8765a756e53e40120959",
+    "url": "/route-editor/index.html"
+  },
+  {
+    "revision": "e1dc63499aa0b997e4d9",
+    "url": "/route-editor/static/js/2.4997967d.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/route-editor/static/js/2.4997967d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2745ba943e7669d16f",
+    "url": "/route-editor/static/js/main.d8d2f71c.chunk.js"
+  },
+  {
+    "revision": "84045c45794496f716fe",
+    "url": "/route-editor/static/js/runtime-main.20ce594d.js"
+  }
+]);
