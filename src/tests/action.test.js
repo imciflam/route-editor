dfdfa24e3ccc0322c757dc80e6b6ccc2526e7102
Vita <vitalia@myrambler.ru>
2020-07-02 21:28:55 +0300

    fixed all tests

--- src/tests/action.test.js
+++ src/tests/action.test.js
@@ -26,3 +26,27 @@ describe('Remove a marker action', () => {
     });
 });
 
+describe('Change position action', () => {
+    it('should add an action to change position of the marker by id', () => {
+        const id = Math.floor(Math.random() * 1000)
+        const position = { lat: 55.6, lng: 51.8 };
+        const expectedAction = {
+            type: types.CHANGE_POSITION,
+            id, position
+        };
+        expect(actions.changePosition(id, position)).toEqual(expectedAction);
+    });
+});
+
+
+describe('Change order action creator', () => {
+    it('should add an action to change order of markers', () => {
+        const markerId = Math.floor(Math.random() * 100)
+        const afterId = Math.floor(Math.random() * 1000)
+        const expectedAction = {
+            type: types.CHANGE_ORDER,
+            markerId, afterId
+        };
+        expect(actions.changeOrder(afterId, markerId)).toEqual(expectedAction);
+    });
+});
